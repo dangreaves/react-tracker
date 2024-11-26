00019ce1be864046307bf3b8009ff46ed5135b77
Dan Greaves <dan@dangreaves.com>
2024-11-26 18:14:38 +1100

    fix: memoize useTracker hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { useAtom, useSetAtom, useAtomValue } from "jotai";
 
 import type { Tracker } from "./types";
@@ -12,25 +13,30 @@ import {
 export function useTracker() {
   useAtom(emitEventsEffect);
 
+  const init = useSetAtom(initAtom);
   const appendEvent = useSetAtom(appendEventAtom);
 
-  return {
-    init: useSetAtom(initAtom),
-    identify: ((...args) =>
-      appendEvent({
-        type: "identify",
-        args,
-      })) as Tracker["identify"],
-    page: ((...args) => appendEvent({ type: "page", args })) as Tracker["page"],
-    track: ((...args) =>
-      appendEvent({ type: "track", args })) as Tracker["track"],
-    group: ((...args) =>
-      appendEvent({ type: "group", args })) as Tracker["group"],
-    alias: ((...args) =>
-      appendEvent({ type: "alias", args })) as Tracker["alias"],
-    reset: ((...args) =>
-      appendEvent({ type: "reset", args })) as Tracker["reset"],
-  };
+  return useMemo(
+    () => ({
+      init,
+      identify: ((...args) =>
+        appendEvent({
+          type: "identify",
+          args,
+        })) as Tracker["identify"],
+      page: ((...args) =>
+        appendEvent({ type: "page", args })) as Tracker["page"],
+      track: ((...args) =>
+        appendEvent({ type: "track", args })) as Tracker["track"],
+      group: ((...args) =>
+        appendEvent({ type: "group", args })) as Tracker["group"],
+      alias: ((...args) =>
+        appendEvent({ type: "alias", args })) as Tracker["alias"],
+      reset: ((...args) =>
+        appendEvent({ type: "reset", args })) as Tracker["reset"],
+    }),
+    [init, appendEvent],
+  );
 }
 
 export function useTrackerState() {
